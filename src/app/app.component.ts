5a5ff6c7f9efd2ac3c8d63d24cbec25d763c1734
cristianmaldonado <maldonadocristian2010@hotmail.com>
2017-09-28 21:46:36 -0300

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Http } from '@angular/Http';
+import 'rxjs/add/operator/toPromise';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+
+
+  lista = [{name: "pepe", age: "12"}, {name: "lucas", age: "23"}];
+  constructor(private http: Http){  }
+
+  ngOnInit(): void {
+    this.http.get("http://localhost:8080/project/flow.flowc?flowActionName=salida").
+    toPromise().then(r => r.json()).then(r => this.lista = r);
+  }
 }
